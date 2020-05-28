485df3e5d7d9635003deb4a932f2f33213ee8cf7
gxk <1005035266@qq.com>
2020-05-28 18:20:07 +0800

    add READMD.md

--- routers/router.go
+++ routers/router.go
@@ -15,7 +15,7 @@ func init() {
 	beego.Router("/account/register/info", &controllers.AccountController{},"Post:PostUserData")
 	beego.Router("/account/login/verify", &controllers.AccountController{},"Post:VerifyUser")
 
-	//员工管
+	//员工管理
 	beego.Router("/user/retrieve/:page", &controllers.UserController{},"Get:GetUserData")
 	beego.Router("/user/create", &controllers.UserController{},"Post:InsertUserData")
 	beego.Router("/user/updata", &controllers.UserController{},"Post:UpdateUserData")
